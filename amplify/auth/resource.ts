24dd4e5d259628653ef6b92a106404d1a72b3a03
AJAY ARUMUGAN <aarumugam@qualityze.com>
2026-06-04 15:40:27 +0530

    sign up

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,5 +1,18 @@
 import { defineAuth } from '@aws-amplify/backend';
 
+/**
+ * Authentication configuration using Amazon Cognito.
+ *
+ * COGNITO ALLOWS UNLIMITED CONCURRENT LOGINS:
+ * - Users can sign in from multiple devices/browsers/tabs simultaneously
+ * - There is no limit on the number of login sessions per user
+ * - Cognito issues access, ID, and refresh tokens for each login
+ * - Multiple refresh tokens can be active at the same time
+ * - By default, access tokens expire after 1 hour, but refresh tokens
+ *   can be used to get new tokens indefinitely (until revoked)
+ *
+ * No changes needed for "many logins" - it's the default behavior.
+ */
 export const auth = defineAuth({
   loginWith: {
     email: true,
